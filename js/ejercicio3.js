6b6271d17ed95b4b07ddfa1a57f4897b9e8ff913
sami27avila <avilasamu27@gmail.com>
2024-07-17 21:22:04 +0200

    Ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,12 @@
+//Ingresa mediante prompt dos valores por teclado y determinar cual de ellos es el mayor e informar mediante un alert
+
+var numero1 = prompt("Ingrese el primer numero")
+var numero2 = prompt("Ingrese el segundo numero")
+
+
+if(numero1 > numero2){
+    alert(numero1+ "es mayor que" +numero2)
+}else{
+    alert(numero2+ "es mayor que" +numero1)
+}
+
